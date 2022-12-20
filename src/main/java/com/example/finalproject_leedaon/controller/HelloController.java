81163e643a1d590cc12d8f9f89afa32f4b248dd6
이다온 <ondayary@gmail.com>
2022-12-20 23:36:35 +0900

    Swagger UI 기능 추가

--- src/main/java/com/example/finalproject_leedaon/controller/HelloController.java
+++ src/main/java/com/example/finalproject_leedaon/controller/HelloController.java
@@ -2,7 +2,6 @@ package com.example.finalproject_leedaon.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
