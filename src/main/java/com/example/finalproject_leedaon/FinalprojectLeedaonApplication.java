8db98f951d71c8bd84f231816cf5ddb66630ee0e
이다온 <ondayary@gmail.com>
2022-12-20 19:50:22 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/finalproject_leedaon/FinalprojectLeedaonApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalproject_leedaon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalprojectLeedaonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalprojectLeedaonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/finalproject_leedaon/FinalprojectLeedaonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalproject_leedaon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectLeedaonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
