0b3c6b108cfd5aeae71c4a0266198a97bcfe360e
이다온 <ondayary@gmail.com>
2022-12-20 23:08:02 +0900

    HelloController 생성

---
+++ src/main/java/com/example/finalproject_leedaon/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.finalproject_leedaon.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/v1/hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok().body("hello");
+    }
+}
