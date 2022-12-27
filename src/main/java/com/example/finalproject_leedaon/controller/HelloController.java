68577766d168c3bd43386d1e95166ddb8cc54463
이다온 <ondayary@gmail.com>
2022-12-27 09:05:09 +0900

    refactor: keyword 변경 darkchocolate -> 이름

--- src/main/java/com/example/finalproject_leedaon/controller/HelloController.java
+++ src/main/java/com/example/finalproject_leedaon/controller/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @GetMapping("/api/v1/hello")
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok().body("darkchocolate");
+        return ResponseEntity.ok().body("이다온");
     }
 }
